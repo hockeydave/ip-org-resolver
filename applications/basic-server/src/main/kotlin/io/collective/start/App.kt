1f4062e609c9f1c17b65207ca6232b4f895d88bd
David Peterson <dpeterson@pebblepost.com>
2023-01-25 11:04:31 -0500

    Change default to port 80 for Heroku

--- applications/basic-server/src/main/kotlin/io/collective/start/App.kt
+++ applications/basic-server/src/main/kotlin/io/collective/start/App.kt
@@ -43,6 +43,6 @@ private fun PipelineContext<Unit, ApplicationCall>.headers(): MutableMap<String,
 
 fun main() {
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
-    val port = System.getenv("PORT")?.toInt() ?: 8888
+    val port = System.getenv("PORT")?.toInt() ?: 80
     embeddedServer(Netty, port, watchPaths = listOf("basic-server"), module = { module() }).start()
 }
